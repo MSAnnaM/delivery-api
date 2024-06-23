1fd5fe1093cb4be567094bd8936b671b466cd51b
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-06-23 13:31:17 +0300

    initial settings

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+
+
+export default app;
